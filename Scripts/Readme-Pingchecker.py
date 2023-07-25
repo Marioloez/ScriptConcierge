51902b7248b0dafba74d947e3b95a1ead3ab092d
Marioloez <48298718+Marioloez@users.noreply.github.com>
2023-07-25 17:52:50 -0600

    Update Readme-Pingchecker.py

--- Scripts/Readme-Pingchecker.py
+++ Scripts/Readme-Pingchecker.py
@@ -1,29 +1,29 @@
-Este código Python utiliza la biblioteca ping3 para monitorear las direcciones IP especificadas en ips_to_monitor. A continuación, se explica brevemente el propósito de cada sección y función:
+Este código Python utiliza la biblioteca `ping3` para monitorear las direcciones IP especificadas en `ips_to_monitor`. A continuación, se explica brevemente el propósito de cada sección y función:
 
-Se importan los módulos necesarios (time, requests y ping3) para realizar el monitoreo y enviar las alertas a Discord.
+- Se importan los módulos necesarios (`time`, `requests` y `ping3`) para realizar el monitoreo y enviar las alertas a Discord.
 
-Se configuran las IPs que se desean monitorear junto con las etiquetas o nombres que se asignarán a cada una de ellas en el diccionario ips_to_monitor.
+- Se configuran las IPs que se desean monitorear junto con las etiquetas o nombres que se asignarán a cada una de ellas en el diccionario `ips_to_monitor`.
 
-ping_interval y alert_interval se utilizan para especificar los intervalos de tiempo en segundos entre cada ping y entre cada alerta, respectivamente.
+- `ping_interval` y `alert_interval` se utilizan para especificar los intervalos de tiempo en segundos entre cada ping y entre cada alerta, respectivamente.
 
-discord_webhook_url contiene la URL de webhook de Discord donde se enviarán las alertas.
+- `discord_webhook_url` contiene la URL de webhook de Discord donde se enviarán las alertas.
 
-send_discord_alert es una función para enviar mensajes de alerta a Discord utilizando la biblioteca requests.
+- `send_discord_alert` es una función para enviar mensajes de alerta a Discord utilizando la biblioteca `requests`.
 
-send_discord_startup_notification es una función para enviar un mensaje de inicio cuando el script se ejecuta por primera vez, informando que ha comenzado a funcionar.
+- `send_discord_startup_notification` es una función para enviar un mensaje de inicio cuando el script se ejecuta por primera vez, informando que ha comenzado a funcionar.
 
-Se crean dos diccionarios: ip_status para mantener el estado actual de cada IP (si responde o no) y last_alert_time para realizar un seguimiento del tiempo de la última alerta enviada para cada IP.
+- Se crean dos diccionarios: `ip_status` para mantener el estado actual de cada IP (si responde o no) y `last_alert_time` para realizar un seguimiento del tiempo de la última alerta enviada para cada IP.
 
-Se envía el mensaje de inicio utilizando la función send_discord_startup_notification().
+- Se envía el mensaje de inicio utilizando la función `send_discord_startup_notification()`.
 
-El bucle while True ejecuta continuamente el monitoreo de las IPs especificadas.
+- El bucle `while True` ejecuta continuamente el monitoreo de las IPs especificadas.
 
-Para cada IP en ips_to_monitor, se realiza un ping utilizando ping3.ping() y se verifica si la IP responde.
+- Para cada IP en `ips_to_monitor`, se realiza un ping utilizando `ping3.ping()` y se verifica si la IP responde.
 
-Si la IP responde, se verifica si ha vuelto a responder después de no haberlo hecho anteriormente. Si es así, se envía un mensaje y se actualiza el estado en ip_status.
+- Si la IP responde, se verifica si ha vuelto a responder después de no haberlo hecho anteriormente. Si es así, se envía un mensaje y se actualiza el estado en `ip_status`.
 
-Si la IP no responde, se verifica si ha pasado suficiente tiempo desde la última alerta (especificado en alert_interval). Si es así, se envía una nueva alerta y se actualiza el tiempo de la última alerta en last_alert_time.
+- Si la IP no responde, se verifica si ha pasado suficiente tiempo desde la última alerta (especificado en `alert_interval`). Si es así, se envía una nueva alerta y se actualiza el tiempo de la última alerta en `last_alert_time`.
 
-Finalmente, el script se pausa durante el intervalo de tiempo especificado en ping_interval antes de continuar con el siguiente ciclo de monitoreo.
+- Finalmente, el script se pausa durante el intervalo de tiempo especificado en `ping_interval` antes de continuar con el siguiente ciclo de monitoreo.
 
-Recuerda reemplazar la URL de webhook de Discord (discord_webhook_url) y las IPs que desees monitorear con sus etiquetas (ips_to_monitor) según tus necesidades. Además, asegúrate de haber instalado la biblioteca ping3 antes de ejecutar el script (pip install ping3).
+Recuerda reemplazar la URL de webhook de Discord (`discord_webhook_url`) y las IPs que desees monitorear con sus etiquetas (`ips_to_monitor`) según tus necesidades. Además, asegúrate de haber instalado la biblioteca `ping3` antes de ejecutar el script (`pip install ping3`).
